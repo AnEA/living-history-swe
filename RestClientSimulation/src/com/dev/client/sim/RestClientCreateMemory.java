ab83709ae4e8adad858b17633c7ec2476a092c66
yikaraman <yikaraman@gmail.com>
2014-12-14 23:38:43 +0200

    Client side Create Memory Simulation

---
+++ RestClientSimulation/src/com/dev/client/sim/RestClientCreateMemory.java
@@ -0,0 +1,69 @@
+
+package com.dev.client.sim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONObject;
+
+public class RestClientCreateMemory {
+
+   public static void main(String[] args) {
+      // String string = "";
+      try {
+
+         // InputStream inputStream = new
+         // FileInputStream("/Users/yikaraman/Desktop/JSONFile.txt");
+         // InputStreamReader isReader = new InputStreamReader(inputStream);
+         // BufferedReader br = new BufferedReader(isReader);
+         // String line;
+         // while ((line = br.readLine()) != null) {
+         // string += line + "\n";
+         // }
+
+         // JSONObject jsonObject = new JSONObject(string);
+         JSONObject jsonObject = new JSONObject();
+         jsonObject.put("place_id", "Berlin");
+         jsonObject.put("author", "ilkerk");
+         jsonObject.put("imageUrl", "http://www.stag-budapest.com/images/katakt/Beer%20bike%20in%20Budapest.jpg");
+         jsonObject.put("content", "A sör (vagy régiesen ser, serital) (a Magyar Élelmiszerkönyv szerint) malátából");
+         jsonObject.put("tags", "beer;budapest,beerbike");
+         jsonObject.put("date", "2014-11-03 00:00:01");
+         jsonObject.put("active", "TRUE");
+         System.out.println(jsonObject);
+
+         try {
+            URL url = new URL("http://localhost:8080/LivingHistoryRestService/rest/memory/creatememory");
+            URLConnection connection = url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+            out.write(jsonObject.toString());
+            out.close();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            System.out.println(in.readLine());
+            while (in.readLine() != null) {
+
+            }
+            System.out.println("\nREST Service Invoked Successfully..");
+            in.close();
+         }
+         catch (Exception e) {
+            System.out.println("\nError while calling REST Service");
+            System.out.println(e);
+         }
+
+         // br.close();
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
