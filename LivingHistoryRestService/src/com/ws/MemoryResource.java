321963724dd94fbb2777b53153d237b927473887
yikaraman <yikaraman@gmail.com>
2014-12-14 23:36:33 +0200

    Create Memory Impl - Get Data from client and insert DB.

--- LivingHistoryRestService/src/com/ws/MemoryResource.java
+++ LivingHistoryRestService/src/com/ws/MemoryResource.java
@@ -1,8 +1,15 @@
 
 package com.ws;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
-import java.sql.*;
+import java.io.InputStreamReader;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
@@ -66,7 +73,7 @@ public class MemoryResource {
                String tags = rsMem.getString("tags");
                Date mem_date = rsMem.getDate("mem_date");
                boolean active = rsMem.getBoolean("active");
-               
+
                JSONObject jObjMem = new JSONObject();
                jObjMem.put("author", author);
                jObjMem.put("imageUrl", image);
@@ -108,4 +115,48 @@ public class MemoryResource {
          return Response.ok(new ErrorResponseBean(e)).build();
       }
    }
+
+   @POST
+   @Path("/creatememory")
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.APPLICATION_JSON)
+   public Response createMemory(InputStream requestBean) {
+      try {
+
+         // If you need to get data from request, you can parse here.
+         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(requestBean));
+         JSONObject obj = new JSONObject(bufferedReader.readLine());
+         String place_id = obj.getString("place_id");
+         String author = obj.getString("author");
+         String imageUrl = obj.getString("imageUrl");
+         String content = obj.getString("content");
+         String tags = obj.getString("tags");
+         String date = obj.getString("date");
+         boolean active = obj.getBoolean("active");
+         
+         SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+         Date startDateParsed = format.parse(date);//parse(date);
+         java.sql.Date startDate = new java.sql.Date(startDateParsed.getTime());
+
+         Connection conn = ConnectDatabase.getInstance().getConnection();
+         CallableStatement stmtMem = null;
+         stmtMem = conn.prepareCall("INSERT INTO memory (place_id, author, image, content, tags, mem_date, active) values(?,?,?,?,?,?,?);");
+         stmtMem.setString(1, place_id);
+         stmtMem.setString(2, author);
+         stmtMem.setString(3, imageUrl);
+         stmtMem.setString(4, content);
+         stmtMem.setString(5, tags);
+         stmtMem.setDate(6, startDate);
+         stmtMem.setBoolean(7, active);
+         System.out.println("end of 1");
+         int row = stmtMem.executeUpdate();
+         System.out.println("end of ");
+         return Response.ok(obj.toString()).build();
+      }
+      catch (Exception e) {
+         System.out.println(e.getMessage());
+         return Response.ok(new ErrorResponseBean(e)).build();
+      }
+   }
+
 }
