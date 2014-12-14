3e58bda0e05b4614c5bc5e22fd03fc7b8b790519
yikaraman <yikaraman@gmail.com>
2014-12-14 20:58:36 +0200

    Get Memories DB, response as JSON Array

--- LivingHistoryRestService/src/com/ws/MemoryResource.java
+++ LivingHistoryRestService/src/com/ws/MemoryResource.java
@@ -1,9 +1,7 @@
 
 package com.ws;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.*;
 
 import javax.ejb.Stateless;
@@ -17,10 +15,9 @@ import javax.ws.rs.core.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.swe.database.ConnectDatabase;
-
 import bean.ErrorResponseBean;
-import bean.UpdateResponseBean;
+
+import com.swe.database.ConnectDatabase;
 
 @Stateless
 @Path("/memory")
@@ -30,9 +27,10 @@ public class MemoryResource {
    @Path("/get")
    @Consumes(MediaType.APPLICATION_JSON)
    @Produces(MediaType.APPLICATION_JSON)
-   public Response getPerson(InputStream requestBean) {
+   public Response getAllMemories(InputStream requestBean) {
       try {
 
+         // If you need to get data from request, you can parse here.
          // BufferedReader bufferedReader = new BufferedReader(new
          // InputStreamReader(requestBean));
          // System.out.println(bufferedReader.readLine());
@@ -45,7 +43,6 @@ public class MemoryResource {
          ResultSet rs = stmt.executeQuery(sql);
 
          JSONArray jArray = new JSONArray();
-
          while (rs.next()) {
             // Retrieve by column name
             String placeName = rs.getString("placeName");
@@ -53,11 +50,39 @@ public class MemoryResource {
             double latitude = rs.getDouble("latitude");
             double longitude = rs.getDouble("longtitude");
 
+            JSONArray jArrayMemory = new JSONArray();
+            // get another query to get Memories.
+            CallableStatement stmtMem = null;
+            System.out.println("Creating statement for Memories of " + placeName);
+            stmtMem = conn.prepareCall("select * from memory where place_id=? ;");
+            stmtMem.setString(1, place);
+            ResultSet rsMem = stmtMem.executeQuery();
+
+            while (rsMem.next()) {
+               String place_id = rsMem.getString("place_id");
+               String author = rsMem.getString("author");
+               String image = rsMem.getString("image");
+               String content = rsMem.getString("content");
+               String tags = rsMem.getString("tags");
+               Date mem_date = rsMem.getDate("mem_date");
+               boolean active = rsMem.getBoolean("active");
+               
+               JSONObject jObjMem = new JSONObject();
+               jObjMem.put("author", author);
+               jObjMem.put("imageUrl", image);
+               jObjMem.put("tags", tags);
+               jObjMem.put("date", mem_date);
+               jObjMem.put("active", active);
+               jArrayMemory.put(jObjMem);
+
+            }
+
             JSONObject jObj = new JSONObject();
             jObj.put("name", placeName);
             jObj.put("place_id", place);
             jObj.put("latitude", latitude);
             jObj.put("longitude", longitude);
+            jObj.put("memories", jArrayMemory);
             jArray.put(jObj);
 
             // Display values
@@ -67,16 +92,16 @@ public class MemoryResource {
             System.out.println(", longitude: " + longitude);
          }
 
-//         // get data, send response
-//         UpdateResponseBean responseBean = new UpdateResponseBean();
-//         responseBean.setName("Boğaziçi Üniversitesi Kuzey Kampüsü");
-//         responseBean.setPlaceID("bogazici_kampus");
-//         responseBean.setLatitude("41.085452");
-//         responseBean.setLongitude("29.044493999999986");
-//         responseBean.setMemories("author");
-//         // insert update to db
-//         System.out.println(responseBean.toString());
-         //return Response.ok(responseBean.toString()).build();
+         // // get data, send response
+         // UpdateResponseBean responseBean = new UpdateResponseBean();
+         // responseBean.setName("Boğaziçi Üniversitesi Kuzey Kampüsü");
+         // responseBean.setPlaceID("bogazici_kampus");
+         // responseBean.setLatitude("41.085452");
+         // responseBean.setLongitude("29.044493999999986");
+         // responseBean.setMemories("author");
+         // // insert update to db
+         // System.out.println(responseBean.toString());
+         // return Response.ok(responseBean.toString()).build();
          return Response.ok(jArray.toString()).build();
       }
       catch (Exception e) {
