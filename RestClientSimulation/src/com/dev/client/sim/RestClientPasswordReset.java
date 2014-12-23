d2d0d04d0e6abd28caebea5fd532a3810da23d34
yikaraman <yikaraman@gmail.com>
2014-12-24 00:08:43 +0200

    Client Simulation for SendEmail and Password Reset

---
+++ RestClientSimulation/src/com/dev/client/sim/RestClientPasswordReset.java
@@ -0,0 +1,49 @@
+package com.dev.client.sim;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONObject;
+
+public class RestClientPasswordReset {
+   
+   public static void main(String[] args) {
+      try {
+         JSONObject jsonObject = new JSONObject();
+         jsonObject.put("email", "ilkerk@gmail.com");
+         System.out.println(jsonObject);
+
+         try {
+            URL url = new URL("http://localhost:8080/LivingHistoryRestService/rest/user/reset");
+            URLConnection connection = url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+            out.write(jsonObject.toString());
+            out.close();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            System.out.println(in.readLine());
+            while (in.readLine() != null) {
+
+            }
+            System.out.println("\nREST Service Invoked Successfully..");
+            in.close();
+         }
+         catch (Exception e) {
+            System.out.println("\nError while calling REST Service");
+            System.out.println(e);
+         }
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+}
