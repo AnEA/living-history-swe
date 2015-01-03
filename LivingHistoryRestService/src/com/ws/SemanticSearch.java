1ea0d99d042d9872e7377f32150769eae0f68d6d
yikaraman <yikaraman@gmail.com>
2015-01-03 03:18:41 +0200

    Semantic Search Comments

--- LivingHistoryRestService/src/com/ws/SemanticSearch.java
+++ LivingHistoryRestService/src/com/ws/SemanticSearch.java
@@ -13,6 +13,7 @@ import java.util.Date;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -37,11 +38,11 @@ import com.swe.database.ConnectDatabase;
 @Path("/search")
 public class SemanticSearch {
 
-   @POST
+   @GET
    @Path("/get")
    @Consumes(MediaType.APPLICATION_JSON)
    @Produces(MediaType.APPLICATION_JSON)
-   public Response getAllResult(InputStream requestBean) {
+   public Response getSemanticResults(InputStream requestBean) {
       try {
 
          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(requestBean));
